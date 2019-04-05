3a33ca853c3fb63ead5c9f78a048fdbfdbc129de
bissols01 <bissoli.marco@iisgalvanimi.edu.it>
2019-04-05 10:27:03 +0000

    cazo

---
+++ HTML/WEB-INF/src/mypkg/DataOra.java
@@ -0,0 +1,40 @@
+package mypkg;
+ 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Date;
+ 
+public class DataOra extends HttpServlet {
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      // Set the response message's MIME type
+      response.setContentType("text/html;charset=UTF-8");
+      // Allocate a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+ 
+      // Write the response message, in an HTML page
+      try {
+          Date date = new java.util.Date();
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title>Hello, World</title></head>");
+         out.println("<body>");
+         out.println("<h1>Hello, world!</h1>");  // says Hello
+         // Echo client's request information
+         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+         // Generate a random number upon each request
+         out.println("<p>A Random Numero: <strong>" + Math.random() + "</strong></p>");
+         out.println("<p style='font-size: 300%;color:red;text-align:center;font-family:Impact;'>Data e orario attuali: <strong>" + date.toString() + "</strong></p>");
+         out.println("</body>");
+         out.println("</html>");
+      } finally {
+         out.close();  // Always close the output writer
+      }
+   }
+}
--- HTML/index.jsp
+++ HTML/index.jsp
@@ -4,6 +4,6 @@
     <title></title>
 </head>
 <body>
-
+<h1 style="text-align:center;color:purple">Benvenuto su BIXE!</h1>
 </body>
 </html>
